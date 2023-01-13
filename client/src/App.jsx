5590ea823e15068a5b5b476d33eaba4645fee325
Purvank Bhiwgade <bhiwgadepurvank@gmail.com>
2023-01-13 19:05:43 +0530

    fy application form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
+import ApplicationForm from "./components/ApplicationForm";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <ApplicationForm />
   );
 }
 
