203c3cb467e52658d6c5bf0998e05d5038236b0b
Purvank Bhiwgade <bhiwgadepurvank@gmail.com>
2023-01-14 06:39:59 +0530

    grievance, modal and table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import ApplicationForm from "./components/ApplicationForm";
 import "./App.css";
 import InfoCollectForm from "./components/InfoCollectForm";
+import GrievanceForm from "./components/GrievanceForm"
+import Table from "./components/Table";
 
 function App() {
+  
   return (
     // <ApplicationForm />
-    <InfoCollectForm />
+    // <InfoCollectForm />
+    // <GrievanceForm />
+    <Table />
   );
 }
 
